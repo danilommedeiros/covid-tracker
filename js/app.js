ddc4a590508683161ea22e734b58737e60148082
Danilo Medeiros <danilo.medeiros801@gmail.com>
2022-02-13 00:28:59 -0300

    add files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+fetch("https://corona.lmao.ninja/v2/countries/Brazil")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        document.getElementById("cases-population").innerHTML = data.population;
+        document.getElementById("cases-cases").innerHTML = data.cases;
+        document.getElementById("cases-active").innerHTML = data.active;
+        document.getElementById("cases-deaths").innerHTML = data.deaths;
+        document.getElementById("cases-tests").innerHTML = data.tests;
+        document.getElementById("cases-recovered").innerHTML = data.recovered;
+    })
+        
+        
